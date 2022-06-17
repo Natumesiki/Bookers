b4db65f8796b0ffaac2b6afcf4755a179a37f4d9
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-06-17 10:47:57 +0000

    bookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,28 @@
 class BooksController < ApplicationController
+
   def new
     @book =Book.new
   end
 
+  def create
+    book = Book.new(book_prams)
+    book.save
+    redirect_to 'top'
+  end
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get 'books/new'
-  get 'books/index'
+  post 'books'=>'books#create'
+  
+  get 'books'  => 'books/index'
   get 'books/show'
   get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get'/top'=>'homes#top'
-# resources :
+
+# root "books#index"
+# resources :books
 end
